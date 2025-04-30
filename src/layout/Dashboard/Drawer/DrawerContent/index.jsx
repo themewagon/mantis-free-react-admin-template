7a52d9673fc6d7dd540cb93a3a937a6dde0cb9b9
AnikaBristy <bristy.technext@gmail.com>
2025-04-30 17:02:48 +0600

    code updated

--- src/layout/Dashboard/Drawer/DrawerContent/index.jsx
+++ src/layout/Dashboard/Drawer/DrawerContent/index.jsx
@@ -1,16 +1,20 @@
-// project import
-import NavCard from './NavCard';
+// project imports
+
 import Navigation from './Navigation';
 import SimpleBar from 'components/third-party/SimpleBar';
+import { useGetMenuMaster } from 'api/menu';
 
 // ==============================|| DRAWER CONTENT ||============================== //
 
 export default function DrawerContent() {
+  const { menuMaster } = useGetMenuMaster();
+  const drawerOpen = menuMaster.isDashboardDrawerOpened;
+
   return (
     <>
       <SimpleBar sx={{ '& .simplebar-content': { display: 'flex', flexDirection: 'column' } }}>
         <Navigation />
-        <NavCard />
+        {drawerOpen}
       </SimpleBar>
     </>
   );
--- src/layout/Dashboard/index.jsx
+++ src/layout/Dashboard/index.jsx
@@ -1,31 +1,29 @@
 import { useEffect } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 
-// material-ui
 import useMediaQuery from '@mui/material/useMediaQuery';
 import Toolbar from '@mui/material/Toolbar';
 import Box from '@mui/material/Box';
 
-// project import
+// project imports
 import Drawer from './Drawer';
 import Header from './Header';
-import navigation from 'menu-items';
+import Footer from './Footer';
 import Loader from 'components/Loader';
 import Breadcrumbs from 'components/@extended/Breadcrumbs';
 
 import { handlerDrawerOpen, useGetMenuMaster } from 'api/menu';
-import { Link, Typography } from '@mui/material';
-import { Stack } from '@mui/system';
 
 // ==============================|| MAIN LAYOUT ||============================== //
 
 export default function DashboardLayout() {
+  const { pathname } = useLocation();
   const { menuMasterLoading } = useGetMenuMaster();
   const downXL = useMediaQuery((theme) => theme.breakpoints.down('xl'));
 
+  // set media wise responsive drawer
   useEffect(() => {
     handlerDrawerOpen(!downXL);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [downXL]);
 
   if (menuMasterLoading) return <Loader />;
@@ -34,24 +32,22 @@ export default function DashboardLayout() {
     <Box sx={{ display: 'flex', width: '100%' }}>
       <Header />
       <Drawer />
+
       <Box component="main" sx={{ width: 'calc(100% - 260px)', flexGrow: 1, p: { xs: 2, sm: 3 } }}>
-        <Toolbar />
-        <Breadcrumbs navigation={navigation} title />
-        <Outlet />
-        <Stack direction="row" justifyContent="space-between" alignItems="center">
-          <Typography variant="subtitle2" sx={{ mt: 3, textAlign: 'right' }}>
-            Made with love by Team
-            <Link ml={0.5} href="https://codedthemes.com">
-              CodedThemes
-            </Link>
-          </Typography>
-          <Typography variant="subtitle2" sx={{ mt: 3, textAlign: 'right' }}>
-            Distributed by
-            <Link ml={0.5} href="https://themewagon.com/">
-              ThemeWagon
-            </Link>
-          </Typography>
-        </Stack>
+        <Toolbar sx={{ mt: 'inherit' }} />
+        <Box
+          sx={{
+            ...{ px: { xs: 0, sm: 2 } },
+            position: 'relative',
+            minHeight: 'calc(100vh - 110px)',
+            display: 'flex',
+            flexDirection: 'column'
+          }}
+        >
+          {pathname !== '/apps/profiles/account/my-account' && <Breadcrumbs />}
+          <Outlet />
+          <Footer />
+        </Box>
       </Box>
     </Box>
   );
